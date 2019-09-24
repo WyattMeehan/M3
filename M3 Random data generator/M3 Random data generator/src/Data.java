5785fcea3a9b6242f8cf7c25728aeab86b10545f
WyattMeehan <43625615+WyattMeehan@users.noreply.github.com>
2019-09-24 18:40:25 -0400

    Add files via upload

---
+++ M3 Random data generator/M3 Random data generator/src/Data.java
@@ -0,0 +1,33 @@
+
+public class Data {
+
+	
+	private String date;
+	private String mac;
+	private String location;
+	
+	
+	
+	//===================== Getters and Setters
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getMac() {
+		return mac;
+	}
+	public void setMacAdrress(String mac) {
+		this.mac = mac;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+}
---
+++ M3 Random data generator/M3 Random data generator/src/RandomDataGenerator.java
@@ -0,0 +1,43 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
+public class RandomDataGenerator {
+
+
+
+
+	public static void main(String[] args){
+
+		RandomAccessFile raf = null;
+		int row = 0;
+		ArrayList<Data> data = new ArrayList<>();
+
+		try {
+			String [] parts;
+			Data d;
+			raf = new RandomAccessFile("data.txt", "r");
+
+			while(raf.getFilePointer() < raf.length()) {
+				row++;
+				parts = raf.readLine().split("\t");
+				d = new Data();
+				d.setDate(parts[0]);
+				d.setMacAdrress(parts[1]);
+				d.setLocation(parts[2]);
+
+				data.add(d);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage() + " on row " + row);
+		} finally {
+			try { raf.close(); } catch (Exception e) {}
+		}
+		
+		for(Data d : data) {
+			System.out.println(d.getMac());
+		}
+	}
+
+}
