639b852f4eeb4fbc4bbf798dd9edb1c84099e05b
lean2 <lean2@miamioh.edu>
2019-10-03 11:57:10 -0400

    done with seperating local and global MAC address

--- src/DataAnalysis/Analysis.java
+++ src/DataAnalysis/Analysis.java
@@ -1,42 +1,66 @@
 package DataAnalysis;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import Anonymization.*;
 
-public class Analysis{
+public class Analysis {
 
     static ArrayList<Data> local = new ArrayList<Data>();
     static ArrayList<Data> global = new ArrayList<Data>();
+    static RandomAccessFile localFile, globalFile;
 
     // saves the data to file
-    public static void save(ArrayList<Data> data, String fileName){
-        RandomAccessFile write = new RandomAccessFile(fileName, "rw");
-        for(int i = 0; i < data.size(); i++) {
-			write.writeBytes(data.get(i).getDate() + "\t" + data.get(i).getMac() + "\t" +
-			data.get(i).getLocation() + "\r\n");
-		}
+    public static void save(Data data, RandomAccessFile write) throws IOException { 
+			write.writeBytes(data.getDate() + "\t" + data.getMac() + "\t" + data.getLocation() + "\r\n");
+    }
+
+    // checks if a hex number has second least significant bit is 1
+    public static boolean checkBit(char hex){
+        if (hex == '2' || hex == '3')
+            return true;
+        if (hex == '6' || hex == '7')
+            return true;
+        if (hex == 'a' || hex == 'b')
+            return true;
+        if (hex == 'e' || hex == 'f')
+            return true;
+        return false;
     }
 
     // seperates globally unique MAC addresses and locally randomized MAC addresses
-    public static void randomSeperate(ArrayList<Data> data){
+    public static void randomSeperate(ArrayList<Data> data) throws IOException {
 
-        // array of characters that have Universally/locally administered address bit is 1
-        char temp[] = {'2', '3', '6', '7', 'A', 'B', 'E', 'F'};
+        // loops through all probe requests
+        for (Data request : data){
 
-        for (Data a : data){
-
-            // gets the second byte of the MAC address
-            char secondByte = a.getMac().charAt(1);
-            if (Arrays.asList
+            if (checkBit(request.getMac().charAt(1)))
+                save(request, localFile);
+            else
+                save(request, globalFile);
             
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
+        // output files
+        localFile = new RandomAccessFile("local.txt", "rw");
+        globalFile = new RandomAccessFile("global.txt", "rw");
         
         // reads file
         ArrayList<Data> data = RandomDataGenerator.readFile("data.txt");
 
+        randomSeperate(data);
+        
+        localFile.close();
+        globalFile.close();
+        
     }
 
 }
