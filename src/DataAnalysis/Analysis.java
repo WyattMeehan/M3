cbfc75ec198894c2a9a1af7fa3e71a0b427e1ee5
anssssss <lean2@miamioh.edu>
2019-10-18 09:49:24 -0400

    done with vendors

--- src/DataAnalysis/Analysis.java
+++ src/DataAnalysis/Analysis.java
@@ -1,28 +1,40 @@
 package DataAnalysis;
 
-import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.RandomAccessFile;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import Anonymization.*;
+import java.util.Hashtable;
+
+import Anonymization.Data;
+import Anonymization.RandomDataGenerator;
 
 public class Analysis {
 
     static ArrayList<Data> local = new ArrayList<Data>();
     static ArrayList<Data> global = new ArrayList<Data>();
     static RandomAccessFile localFile, globalFile;
+    static Hashtable<String, String> table;
+
+    // formats MAC address
+    public static String format(String MAC){
+        return MAC.replaceAll(":", "").toUpperCase();
+    }
 
     // saves the data to file
-    public static void save(Data data, RandomAccessFile write) throws IOException {
+    public static void save(Data data) throws IOException {
 
-        // interprets MAC addresses
-        String MAC = data.getMac();
-        String response[] = lookup(MAC);
+        // interprets MAC addressesS
+        String MAC = format(data.getMac());
 
-        write.writeBytes(data.getDate() + "\t" + MAC + "\t" + data.getLocation() + "\t" + getVendor(response[1]) + "\r\n");
+        if (checkBit(MAC.charAt(1)))
+            localFile.writeBytes(data.getDate() + "\t" + MAC + "\t" + data.getLocation() + "\r\n");
+        else {
+            String vendor = table.get(MAC.substring(0, 6));
+            globalFile.writeBytes(data.getDate() + "\t" + MAC + "\t" + data.getLocation() + "\t" + vendor + "\r\n");
+        }
+
     }
 
     // checks if a hex number has second least significant bit is 1
@@ -38,61 +50,59 @@ public class Analysis {
         return false;
     }
 
-    // looks up vendor
-    // returns response code + content
-    public static String[] lookup(String MAC) throws IOException {
+    /*
+     * // looks up vendor // returns response code + content public static String[]
+     * lookup(String MAC) throws IOException {
+     * 
+     * String result[] = new String[2];
+     * 
+     * // sets url String url = "https://mac2vendor.com/api/v4/mac/" + MAC; URL api
+     * = new URL(url);
+     * 
+     * // initiates connection HttpURLConnection connect =
+     * (HttpURLConnection)api.openConnection();
+     * connect.addRequestProperty("User-Agent",
+     * "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+     * 
+     * // gets response code int code = connect.getResponseCode(); result[0] = "" +
+     * code;
+     * 
+     * // gets response BufferedReader in = new BufferedReader(new
+     * InputStreamReader(connect.getInputStream())); String inputLine; StringBuffer
+     * response = new StringBuffer(); while ((inputLine = in.readLine()) != null)
+     * response.append(inputLine); result[1] = response.toString(); in.close();
+     * 
+     * return result; }
+     * 
+     * // gets vendor name from API response public static String getVendor(String
+     * response){
+     * 
+     * // trims the response then gets the vendor name using location in response
+     * return response.substring(response.indexOf("vendor") + 10,
+     * response.indexOf("mac") - 15);
+     * 
+     * // NOTE: LAA means Locally Administered Addresses
+     * 
+     * }
+     * 
+     * // seperates globally unique MAC addresses and locally randomized MAC
+     * addresses public static void randomSeperate(ArrayList<Data> data) throws
+     * IOException {
+     * 
+     * // loops through all probe requests for (Data request : data){
+     * 
+     * if (checkBit(request.getMac().charAt(1))) save(request, localFile); else
+     * save(request, globalFile);
+     * 
+     * } }
+     */
 
-        String result[] = new String[2];
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        // sets url
-        String url = "https://mac2vendor.com/api/v4/mac/" + MAC;
-        URL api = new URL(url);
-
-        // initiates connection
-        HttpURLConnection connect = (HttpURLConnection)api.openConnection();
-        connect.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-
-        // gets response code
-        int code = connect.getResponseCode();
-        result[0] = "" + code;
-
-        // gets response
-        BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null)
-            response.append(inputLine);
-        result[1] = response.toString();
-        in.close();
-
-        return result;
-    }
-
-    // gets vendor name from API response
-    public static String getVendor(String response){
-
-        // trims the response then gets the vendor name using location in response
-        return response.substring(response.indexOf("vendor") + 10, response.indexOf("mac") - 15);
-
-        // NOTE: LAA means Locally Administered Addresses
-
-    }
-
-    // seperates globally unique MAC addresses and locally randomized MAC addresses
-    public static void randomSeperate(ArrayList<Data> data) throws IOException {
-
-        // loops through all probe requests
-        for (Data request : data){
-
-            if (checkBit(request.getMac().charAt(1)))
-                save(request, localFile);
-            else
-                save(request, globalFile);
-            
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+        // loads hash table
+        FileInputStream fis = new FileInputStream("oui.tmp");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        table = (Hashtable<String, String>) ois.readObject();
 
         // output files
         localFile = new RandomAccessFile("local.txt", "rw");
@@ -101,10 +111,13 @@ public class Analysis {
         // reads file
         ArrayList<Data> data = RandomDataGenerator.readFile("data.txt");
 
-        randomSeperate(data);
+        for (Data d : data)
+            save(d);
+
         
         localFile.close();
         globalFile.close();
+        ois.close();
         
     }
 
--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,13 @@
 // package for testing
 
 import java.io.IOException;
-import DataAnalysis.*;
+
+import DataAnalysis.Analysis;
 
 public class Test {
 
     public static void main(String[] args) throws IOException {
-        String result[] = Analysis.lookup("de:0d:90:53:4e:2c");
-        String vendor = Analysis.getVendor(result[1]);
-        System.out.println(result[1]);
-        System.out.println(vendor.trim());
-        System.out.println(vendor.length());
+        System.out.println(Analysis.format("94:16:25:c1:90:01"));
     }
     
 }
