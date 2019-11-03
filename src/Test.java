2cdfa65bb2fd9529dbf7f1349b2d46436a8a19a7
anssssss <lean2@miamioh.edu>
2019-11-03 15:52:05 -0500

    tested program to remove people who were seen a long time ago

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 // package for testing
-
-
 
 public class Test {
 
-    public static void main(String[] args) {
-        String response = "0";
-        String last = "1";
-        
-        while (!response.equals(last)){
+    public static void main(String[] args) throws IOException, ParseException {
 
+        // gets response
+        URL url = new URL("http://csebu.csi.miamioh.edu/cmx/v1/AABFG/activeCount/Oxford%3EBenton%20Hall%3E2nd%20Floor");
+        HttpURLConnection connect = (HttpURLConnection)url.openConnection();
+        connect.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+        BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream())); 
+        String inputLine;
+        StringBuffer response = new StringBuffer(); 
+        while ((inputLine = in.readLine()) != null)
+            response.append(inputLine); 
+        String result = response.toString(); 
+        in.close();
+
+        // current time
+        long time = System.currentTimeMillis();
+
+        // date formatter
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        
+        int index = 0;
+        int count = 0;
+        int range = 15;
+
+        // finds all "lastSeen" in response
+        while (result.indexOf("lastSeen", index + 1) > 0){
+            index = result.indexOf("lastSeen", index + 1);
+
+            // finds date data
+            String date = result.substring(index + 11, index + 30).replace("T", " ");
+
+            if (date.charAt(0) == '2'){
+
+                // converts date to ms
+                Date d = format.parse(date);
+                long ms = d.getTime();
+
+                if (time - ms < 1000 * range){
+                    count++;
+                    System.out.println(date);
+                }
+            }
+            
         }
+
+        System.out.println("\n\n" + count + "people in " + range + " s");
     }
     
 }
